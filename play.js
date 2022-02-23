b8b54a319482944ca2dd676adda816aaa9596ed5
Harjas101 <anand.harjas@gmail.com>
2022-02-23 10:28:07 -0500

    module export added in client

--- play.js
+++ play.js
@@ -1,22 +1,22 @@
 const net = require("net");
-
+const connect = require("./client");
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost' ,
-    port: 50541
-  });
-conn.on("data" , (data) => {
-  console.log("server says : ", data)
-})
-conn.on("connect", () => {
-  // code that does something when the connection is first established
-  console.log('Successfully connected to the game server');
-});
-  conn.setEncoding("utf8");
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: 'localhost' ,
+//     port: 50541
+//   });
+//   conn.on("data" , (data) => {
+//     console.log("server says : ", data)
+//   })
+//   conn.on("connect", () => {
+//     // code that does something when the connection is first established
+//     console.log('Successfully connected to the game server');
+//   });
+//   conn.setEncoding("utf8");
 
-  return conn;
-};
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
