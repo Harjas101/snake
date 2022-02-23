30adbff795f2c9f904c23cc79c41f48a4d48b84a
Harjas101 <anand.harjas@gmail.com>
2022-02-23 13:23:43 -0500

    added input file and export to play

---
+++ input.js
@@ -0,0 +1,24 @@
+let connection;
+const inputObject = {'w': 'Move: up', 'a': 'Move: left', 's': 'Move: down', 'd': 'Move: right'}
+
+const handleUserInput = function (key) {
+     if (key === '\u0003') {
+     process.exit();
+   }
+
+   if (inputObject.hasOwnProperty(key)){
+    connection.write(inputObject[key]);
+   }
+};
+
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,6 +1,8 @@
 
-const connect = require("./client");
-
+const net = require("net");
+const connect = require('./client.js')
+const setupInput = require('./input.js')
 
 console.log("Connecting ...");
-connect();
+//connect();
+setupInput(connect());
