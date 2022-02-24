6a699087a1e8bc5ac884c56d641a971429a1fa90
Harjas101 <anand.harjas@gmail.com>
2022-02-24 08:13:12 -0500

    added messages x,z

--- input.js
+++ input.js
@@ -1,5 +1,5 @@
 let connection;
-const inputObject = {'w': 'Move: up', 'a': 'Move: left', 's': 'Move: down', 'd': 'Move: right'}
+const inputObject = {'w': 'Move: up', 'a': 'Move: left', 's': 'Move: down', 'd': 'Move: right','x': "Say: hellomoto", 'z': "Say: sup"}
 
 const handleUserInput = function (key) {
      if (key === '\u0003') {
