a260b29215d5c18570907397909ced7c5b9096e7
Harjas101 <anand.harjas@gmail.com>
2022-02-24 12:17:59 -0500

    linted properly

--- input.js
+++ input.js
@@ -1,5 +1,5 @@
 let connection;
-const inputObject = {'w': 'Move: up', 'a': 'Move: left', 's': 'Move: down', 'd': 'Move: right','x': "Say: hellomoto", 'z': "Say: sup"}
+const { inputObject } = require("./constants");
 
 const handleUserInput = function (key) {
      if (key === '\u0003') {
